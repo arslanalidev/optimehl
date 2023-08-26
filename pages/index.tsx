383a1a404ee5da983f49beb68a7cad39bcc8a8e9
Viet Duc Kieu <viet.duc.kieu@web.de>
2023-08-26 15:00:07 +0200

    add stats in inventory page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,115 @@
-import { HeroTitle } from '../components/HeroTitle';
+import { motion } from "framer-motion";
+import { useRouter } from 'next/router';
+import {
+  Card, Container, Grid, Text, Title,
+  Image, Button, Space
+} from '@mantine/core'
+import React from 'react'
 
 export default function Home() {
+  const router = useRouter()
+
   return (
-    <>
-      <main>
-        <HeroTitle />
-      </main>
-    </>
-  )
+    <Container mih={"80vh"} mt={"5vh"}>
+      <motion.div
+        initial={{ opacity: 0, y: -10 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.8 }}
+      >
+        <Title order={1} align="center" >Bäckerei{" "}
+          <Text component="span" variant="gradient" gradient={{ from: "#FF0000", to: "#FF7878" }}>
+            Spontan
+          </Text>
+        </Title>
+      </motion.div>
+
+      <Space h={70} />
+
+      <motion.div
+        initial={{ opacity: 0, y: 10 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.8, delay: 0.4, ease: "easeIn" }}
+      >
+        <Grid gutter={"xl"}>
+          <Grid.Col xs={12} md={6} lg={4}>
+            <Card shadow="sm" p={1} radius="md" withBorder>
+              <Card.Section>
+                <Image
+                  src="/people.png"
+                  height={160}
+                  alt="Menschenmenge"
+                  fit="contain"
+                />
+              </Card.Section>
+
+              <Text pt={10} weight={500} align="center">Personalplanung</Text>
+
+              <Text size="sm" color="dimmed" align="center">
+                Mitarbeiter können ihren Dienstplan einsehen und Urlaub beantragen
+              </Text>
+
+              <Button
+                variant="light" color="red" fullWidth mt="md" radius="md"
+                onClick={() => { router.push("/personal") }}
+              >
+                Personal planen
+              </Button>
+            </Card>
+          </Grid.Col>
+
+          <Grid.Col xs={12} md={6} lg={4}>
+            <Card shadow="sm" p={1} radius="md" withBorder>
+              <Card.Section>
+                <Image
+                  src="/production.png"
+                  height={160}
+                  alt="Produktion"
+                  fit="contain"
+                />
+              </Card.Section>
+
+              <Text pt={10} weight={500} align="center">Produktionsleitung</Text>
+
+              <Text size="sm" color="dimmed" align="center">
+                Planung welche Produkte wann produziert werden sollen
+              </Text>
+
+              <Button
+                variant="light" color="red" fullWidth mt="md" radius="md"
+                onClick={() => { router.push("/produktionsleitung") }}
+              >
+                Produktion planen
+              </Button>
+            </Card>
+          </Grid.Col>
+
+          <Grid.Col xs={12} md={6} lg={4}>
+            <Card shadow="sm" p={1} radius="md" withBorder>
+              <Card.Section>
+                <Image
+                  src="/lager.png"
+                  height={160}
+                  alt="Lager"
+                  fit="contain"
+                />
+              </Card.Section>
+
+              <Text pt={10} weight={500} align="center">Lagerwesen</Text>
+
+              <Text size="sm" color="dimmed" align="center">
+                Bestandverwaltung und Planung der Bestellungen
+              </Text>
+
+              <Button
+                variant="light" color="red" fullWidth mt="md" radius="md"
+                onClick={() => { router.push("/lagerwesen") }}
+              >
+                Waren bestellen
+              </Button>
+            </Card>
+          </Grid.Col>
+        </Grid>
+      </motion.div>
+    </Container>
+  );
 }
--- pages/lagerwesen.tsx
+++ pages/lagerwesen.tsx
@@ -1,8 +1,39 @@
 import React from 'react'
+import { motion } from "framer-motion";
+import {
+    Container
+} from '@mantine/core'
+import { StatsGridIcons } from '../components/StatsGridIcons'
 
 const Lagerwesen = () => {
     return (
-        <div>Lagerwesen</div>
+        <motion.div
+            initial={{ opacity: 0, y: -10 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.8 }}
+        >
+
+            <Container mih={"80vh"} mt={"5vh"}>
+                <StatsGridIcons data={[
+                    {
+                        "title": "Revenue",
+                        "value": "$13,456",
+                        "diff": 34
+                    },
+                    {
+                        "title": "Profit",
+                        "value": "$4,145",
+                        "diff": -13
+                    },
+                    {
+                        "title": "Coupons usage",
+                        "value": "745",
+                        "diff": 18
+                    }
+                ]
+                } />
+            </Container>
+        </motion.div>
     )
 }
 
